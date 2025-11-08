048a70e205d072036be591df35a1ce5825eecf0b
Alec Hutchinson <alecmhutchinson@gmail.com>
2025-11-07 20:12:43 -0500

    completed Dark-Echoes

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,60 @@
-export default function App() {
+import { useState } from "react";
   // TODO
+
+
+export default function App() {
+  const [episodes] = useState(episodeList);
+  const [selectedEpisode, setSelectedEpisode] = useState();
+
+  /** Details about the selected episode */
+  function EpisodeDetails() {
+    if (!selectedEpisode) {
+      return (
+        <section className="details">
+          <h2>Episode Details</h2>
+          <p>Select an episode to learn more.</p>
+        </section>
+      );
+    }
+    return (
+      <section className="details">
+        <h2>Episode {selectedEpisode.id}</h2>
+        <h3>{selectedEpisode.title}</h3>
+        <p>{selectedEpisode.description}</p>
+        <button>Watch now</button>
+      </section>
+    );
+  }
+
+  /** A list of episode names that allows the user to select an episode */
+  function EpisodeList() {
+    return (
+      <section className="episodes">
+        <h2>Episodes</h2>
+        <ol>
+          {episodes.map((episode) => (
+            <li
+              key={episode.id}
+              onClick={() => setSelectedEpisode(episode)}
+              className={selectedEpisode?.id === episode.id ? "selected" : ""}
+            >
+              {episode.title}
+            </li>
+          ))}
+        </ol>
+      </section>
+    );
+  }
+
+  return (
+    <>
+      <header>
+        <h1>Dark Echoes</h1>
+      </header>
+      <main>
+        <EpisodeList />
+        <EpisodeDetails />
+      </main>
+    </>
+  );
 }
